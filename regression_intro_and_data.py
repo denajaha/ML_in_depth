d624034afeb458ac2c7498178fb0b8d89ae820f8
denis <e1527687@student.tuwien.ac.at>
2020-08-27 12:35:50 +0200

    tut2 done, start with matplotlib and pandas data analysis, brb

--- regression_intro_and_data.py
+++ regression_intro_and_data.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import quandl
+import math
 '''
 YOUR API KEY IS:
 jFfpEMy821y2-sxAZ8uz
@@ -14,5 +15,12 @@ df['HL_PCT'] = (df['Adj. High'] - df['Adj. Close']) / df['Adj. Close'] * 100.0
 df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100.0
 
 df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
+
+forecast_col = 'Adj. Close'
+df.fillna(-99999, inplace=True)
+
+forecast_out = int(math.ceil(0.1+len(df)))
+
+df['label'] = df[forecast_col].shift(-forecast_out)
 
 print(df.head())
--- testing_grounds.py
+++ testing_grounds.py
@@ -1,1 +1,9 @@
-print('hello')
+import requests
+
+csv_url = 'https://media.githubusercontent.com/media/molecularsets/moses/master/data/dataset_v1.csv'
+req = requests.get(csv_url)
+
+csv_file = open('downloaded.csv', 'wb')
+
+csv_file.write(req)
+csv_file.close()
