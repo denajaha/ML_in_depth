ac5f5516659c28c31f8634c77151a1d446885a9a
denis <e1527687@student.tuwien.ac.at>
2020-08-28 15:03:17 +0200

    stuff added from the couple of videos

--- testing_grounds.py
+++ testing_grounds.py
@@ -1,9 +1,19 @@
 import requests
 
-csv_url = 'https://media.githubusercontent.com/media/molecularsets/moses/master/data/dataset_v1.csv'
+# csv_url = 'https://media.githubusercontent.com/media/molecularsets/moses/master/data/dataset_v1.csv'
+csv_url = 'http://zinc15.docking.org/substances/ZINC000000000007/'
 req = requests.get(csv_url)
+print(req.text)
 
-csv_file = open('downloaded.csv', 'wb')
+# csv_file = open('downloaded.csv', 'wb')
+#
+# csv_file.write(req)
+# csv_file.close()
 
-csv_file.write(req)
-csv_file.close()
+
+data = {
+  'page.format': 'properties'
+}
+
+response = requests.post('http://zinc.docking.org/substances/', data=data)
+#print(response.text)
