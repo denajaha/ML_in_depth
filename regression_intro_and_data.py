f08a6ce6275987a618c298ec33eabc93a1626f57
denis <e1527687@student.tuwien.ac.at>
2020-09-05 20:18:04 +0200

    tut 11 done, testing for the linear regression is left and then moving on

--- regression_intro_and_data.py
+++ regression_intro_and_data.py
@@ -1,11 +1,18 @@
 import pandas as pd
-import quandl
-import math
+import quandl, math, datetime
+import numpy as np
+from sklearn import preprocessing, model_selection, svm
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+from matplotlib import style
+import pickle
+
+style.use('ggplot')
 '''
 YOUR API KEY IS:
 jFfpEMy821y2-sxAZ8uz
+# pickel is serialisation of any python object (dict, clf...)
 '''
-
 
 api_key = quandl.ApiConfig.api_key = "jFfpEMy821y2-sxAZ8uz"
 df = quandl.get('WIKI/GOOGL')
@@ -19,8 +26,56 @@ df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
 forecast_col = 'Adj. Close'
 df.fillna(-99999, inplace=True)
 
-forecast_out = int(math.ceil(0.1+len(df)))
+forecast_out = int(math.ceil(0.01 * len(df)))
+print(forecast_out)
 
 df['label'] = df[forecast_col].shift(-forecast_out)
+# feature --> X
+# labels --> y
+X = np.array(df.drop(['label'], 1))
+X = preprocessing.scale(X)
 
-print(df.head())
+X_lately = X[-forecast_out:]
+X = X[:-forecast_out]
+df.dropna(inplace=True)
+
+y = np.array(df['label'])
+
+X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2)
+
+# fit the classifier
+# clf = svm.SVR(kernel='poly')
+clf = LinearRegression(n_jobs=-1)
+clf.fit(X_train, y_train)  # fit --> train
+# choose a place where you want to save your classifier in pickle, so you do not need to train whole thing all over again
+with open('linearregression.pickle', 'wb') as f:
+    pickle.dump(clf, f)
+
+pickle_in = open('linearregression.pickle', 'rb')
+clf = pickle.load(pickle_in)
+accuracy = clf.score(X_test, y_test)  # score --> test
+
+forecast_set = clf.predict(X_lately)
+
+print(forecast_set, accuracy, forecast_out)
+
+df['Forecast'] = np.nan
+# loc gets rows (or columns) with particular labels from the index.
+# iloc gets rows (or columns) at particular positions in the index (so it only takes integers).
+# https://stackoverflow.com/questions/31593201/how-are-iloc-and-loc-different
+last_date = df.iloc[-1].name
+last_unix = last_date.timestamp()
+one_day = 86400
+next_unix = last_unix + one_day
+
+for i in forecast_set:
+    next_date = datetime.datetime.fromtimestamp(next_unix)
+    next_unix += one_day
+    df.loc[next_date] = [np.nan for _ in range(len(df.columns) - 1)] + [i]
+
+df['Adj. Close'].plot()
+df['Forecast'].plot()
+plt.legend(loc=4)
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.show()
